bbf9f6273b1d9d7f578984b64c74ee146ecd3116
planck <987379867@qq.com>
2024-09-14 15:41:56 +0800

    add ostta with ema

--- cifar/run_ema.sh
+++ cifar/run_ema.sh
@@ -16,10 +16,10 @@ done
 
 
 
-CUDA_VISIBLE_DEVICES=$gpu python main.py --adaptation ostta --dataset $dataset --save_dir "./output/ema" 
 # Tent, EATA, OSTTA
-for gamma in 0.9 0.95 0.99 0.995 0.999 ; do
+for gamma in 1 0.9 0.95 0.99 0.995 0.999 ; do
   CUDA_VISIBLE_DEVICES=$gpu python main.py --adaptation ostta_ema --gamma $gamma --dataset $dataset --save_dir "./output/ema" 
 done
 
+CUDA_VISIBLE_DEVICES=$gpu python main.py --adaptation ostta --dataset $dataset --save_dir "./output/ema" 
 
