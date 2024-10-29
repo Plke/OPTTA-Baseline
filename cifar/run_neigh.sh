994aa418c283913b5347483ec9bb586569b95ff2
planck <987379867@qq.com>
2024-10-29 11:35:07 +0800

    全局加上局部

---
+++ cifar/run_neigh.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -Eeuxo pipefail
+
+while getopts ":g:d:" opt; do
+    # shellcheck disable=SC2220
+    case "$opt" in
+        g)
+            gpu="$OPTARG"
+            ;;
+        d)
+            dataset="$OPTARG"
+            ;;
+    esac
+done
+
+
+
+for adaptation in ostta_neigh ; do
+    for alpha1 in 1.0 0.5 0.2 0.1 0; do
+        for alpha2 in 1.0 0.5 0.2 0.1 0; do 
+            CUDA_VISIBLE_DEVICES=$gpu python main.py --adaptation $adaptation  --dataset $dataset --save_dir "./output/${dataset}/${adaptation}" --alpha $alpha1 $alpha2
+        done
+    done
+done
+
+
+
