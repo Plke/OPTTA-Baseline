40bd138f19371f943b7954fce654e836e3ba23a9
planck <987379867@qq.com>
2024-11-04 11:08:54 +0800

    add batch_size

--- cifar/run_neigh.sh
+++ cifar/run_neigh.sh
@@ -17,10 +17,13 @@ done
 
 
 for adaptation in ostta_neigh ; do
+for batch_size in 100 200 400; do
     for alpha1 in 1.0 0.5 0.2 0.1 0; do
         for alpha2 in 1.0 0.5 0.2 0.1 0; do 
-            CUDA_VISIBLE_DEVICES=$gpu python main.py --adaptation $adaptation  --dataset $dataset --save_dir "./output/${dataset}/${adaptation}" --alpha $alpha1 $alpha2
+            CUDA_VISIBLE_DEVICES=$gpu python main.py --adaptation $adaptation  --dataset $dataset --save_dir "./output/${dataset}/${adaptation}" --alpha $alpha1 $alpha2 --batch_size $batch_size
+
         done
+    done
     done
 done
 
